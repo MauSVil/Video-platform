3e1ba837ed6bf9faae09e5a0f3942bb7b948af22
MauSVil <maujr10@hotmail.com>
2019-03-29 13:36:46 -0600

    Playlists added

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Media from './src/playlist/components/media';
-import data from './src/api.json'
-import Playlist from './src/playlist/components/playlist'
-
-const app = document.getElementById('app');
-render(<Playlist data={data}/>, app)
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,38 +1,18 @@
 import React, {Component} from 'react'
 import Media from './media'
 import './playlist.css'
-import Play from '../../icons/components/play'
-import Pause from '../../icons/components/pause'
-import Volume from '../../icons/components/volume'
-import FullScreen from '../../icons/components/full-screen'
 
-class Playlist extends Component{
-    render(){
-        const playlist = this.props.data.categories[0].playlist
-        console.log(this.props.data);
-        
-        return(
-            <div className="Playlist">
-                <Play 
-                size={50}
-                color="red"/>
-                <Pause 
-                size={50}
-                color="red"/>
-                <Volume 
-                size={50}
-                color="red"/>
-                <FullScreen
-                size={50}
-                color="red"/>
-                {
-                    playlist.map((item)=>{
-                        return <Media {...item} key={item.id} />
-                    })
-                }
-            </div>
-        )
-    }
-}
+function Playlist(props){
+    return(
+        <div className="Playlist">
+            {
+                props.playlist.map((item)=>{
+                    return <Media {...item} key={item.id} />
+                })
+            }
+        </div>
+    )
+}        
+    
 
 export default Playlist
