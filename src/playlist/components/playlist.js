6707ae88101b38bc78e75217ebce2f2846978a55
MauSVil <maujr10@hotmail.com>
2019-03-29 12:53:21 -0600

    Icons added

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import Media from './media'
 import './playlist.css'
+import Play from '../../icons/components/play'
+import Pause from '../../icons/components/pause'
+import Volume from '../../icons/components/volume'
+import FullScreen from '../../icons/components/full-screen'
 
 class Playlist extends Component{
     render(){
@@ -9,6 +13,18 @@ class Playlist extends Component{
         
         return(
             <div className="Playlist">
+                <Play 
+                size={50}
+                color="red"/>
+                <Pause 
+                size={50}
+                color="red"/>
+                <Volume 
+                size={50}
+                color="red"/>
+                <FullScreen
+                size={50}
+                color="red"/>
                 {
                     playlist.map((item)=>{
                         return <Media {...item} key={item.id} />
