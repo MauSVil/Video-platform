8fd0caeaef5d1b8255057a282496a85869abb1eb
MauSVil <maujr10@hotmail.com>
2019-03-29 01:26:56 -0600

    State && SetState

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
-    // constructor(props){
-    //     super(props)
-    //     this.handleClick = this.handleClick.bind(this);
-    // }
-    handleClick= (event)=> {
-        console.log(this.props.image);    
+    state = {
+        author: 'Mauricio Sanchez'
     }
-    
+    handleClick= (event)=> {
+        // console.log(this.props.image); 
+        this.setState({
+            author: 'Leonidas Esteban',
+        })   
+    }
+
     render() {
     return (
       <div className="Media" onClick={this.handleClick}>
         <div className= "Media-cover">
           <img className="Media-image" src={this.props.image} alt="" width={260} height={160} />
           <h3 className="Media-title">{this.props.title}</h3>
-          <p className="Media-autor">{this.props.author}</p>
+          <p className="Media-autor">{this.state.author}</p>
         </div>
       </div>
     );
